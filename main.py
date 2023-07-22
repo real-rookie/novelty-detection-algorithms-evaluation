9d96c304bcd5cfeacdfad2a6857db9ed0c227250
Zhiwen Liu <90580132+real-rookie@users.noreply.github.com>
2023-07-21 20:12:17 -0600

    new method: inter_set

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ if __name__ == '__main__':
                         help='train or test')
     parser.add_argument('--weight', default=None,
                         help='weight file')
+    parser.add_argument('--method', default="one_to_many",
+                        help='["one_to_many", "inter_set", "set_to_set"]')
     args = parser.parse_args()
     
     data = args.data
     model = args.model
     mode = args.mode
+    method = args.method
 
     num_of_classes = {
         "MNIST": 10, "FashionMNIST": 10, "CIFAR10": 10, "MVTec-AD": 15
@@ -24,13 +27,21 @@ if __name__ == '__main__':
 
     if mode == 'train':
         if model in ["RD4AD", "patchcore", "patchcore_resnet50"]:
-            for normal_cls in range(num_of_classes[data]):
-                os.system(f'python anomalib/tools/train.py --config config/{model}/{data}/{model}_{data}_{normal_cls}.yaml')
-    
+            if method == "one_to_many":
+                for normal_cls in range(num_of_classes[data]):
+                    os.system(f'python anomalib/tools/train.py --config config/{model}/{data}/{model}_{data}_{normal_cls}.yaml')
+            elif method == "inter_set":
+                os.system(f'python anomalib/tools/train.py --config config/{model}/{data}/{model}_{data}_inter_set.yaml')
     elif mode == 'test':
         if model in ["RD4AD", "patchcore", "patchcore_resnet50"]:
-            for normal_cls in range(num_of_classes[data]):
+            if method == "one_to_many":
+                for normal_cls in range(num_of_classes[data]):
+                    if model == "RD4AD":
+                        os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_{normal_cls}_test.yaml --weight_file results/{model}_{data}_{normal_cls}/reverse_distillation/{data}_{normal_cls}/run/weights/lightning/model.ckpt')
+                    else:
+                        os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_{normal_cls}_test.yaml --weight_file results/{model}_{data}_{normal_cls}/{model}/{data}_{normal_cls}/run/weights/lightning/model.ckpt')
+            elif method == "inter_set":
                 if model == "RD4AD":
-                    os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_{normal_cls}_test.yaml --weight_file results/{model}_{data}_{normal_cls}/reverse_distillation/{data}_{normal_cls}/run/weights/lightning/model.ckpt')
-                    continue
-                os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_{normal_cls}_test.yaml --weight_file results/{model}_{data}_{normal_cls}/{model}/{data}_{normal_cls}/run/weights/lightning/model.ckpt')
+                    os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_inter_set_test.yaml --weight_file results/{model}_{data}_inter_set_test/reverse_distillation/{data}_inter_set_test/run/weights/lightning/model.ckpt')
+                else:
+                    os.system(f'python anomalib/tools/test.py --model {model} --config config/{model}/{data}/{model}_{data}_inter_set_test.yaml --weight_file results/{model}_{data}_inter_set_test/{model}/{data}_inter_set_test/run/weights/lightning/model.ckpt')
